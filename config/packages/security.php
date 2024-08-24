e33b23e48fda0fc04c3ce651420d602f9245dbdf
Thomas Agé <thomas.age@protonmail.com>
2024-08-24 10:49:43 -0400

    feat: Convert config from YAML to PHP format

---
+++ config/packages/security.php
@@ -0,0 +1,63 @@
+<?php
+
+declare(strict_types=1);
+
+use App\Entity\User;
+use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
+
+return static function (ContainerConfigurator $containerConfigurator): void {
+    $containerConfigurator->extension('security', [
+        'password_hashers' => [
+            PasswordAuthenticatedUserInterface::class => 'auto',
+        ],
+        'providers' => [
+            'app_user_provider' => [
+                'entity' => [
+                    'class' => User::class,
+                    'property' => 'email',
+                ],
+            ],
+        ],
+        'firewalls' => [
+            'dev' => [
+                'pattern' => '^/(_(profiler|wdt)|css|images|js)/',
+                'security' => false,
+            ],
+            'main' => [
+                'lazy' => true,
+                'provider' => 'app_user_provider',
+                'form_login' => [
+                    'login_path' => 'app_login',
+                    'check_path' => 'app_login',
+                    'enable_csrf' => true,
+                ],
+                'logout' => [
+                    'path' => 'app_logout',
+                ],
+            ],
+        ],
+        'access_control' => [
+            [
+                'path' => '/login',
+                'roles' => 'PUBLIC_ACCESS',
+            ],
+            [
+                'path' => '^/',
+                'roles' => 'ROLE_USER',
+            ],
+        ],
+    ]);
+    if ('test' === $containerConfigurator->env()) {
+        $containerConfigurator->extension('security', [
+            'password_hashers' => [
+                PasswordAuthenticatedUserInterface::class => [
+                    'algorithm' => 'auto',
+                    'cost' => 4,
+                    'time_cost' => 3,
+                    'memory_cost' => 10,
+                ],
+            ],
+        ]);
+    }
+};
---
+++ config/packages/zenstruck_foundry.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+
+return static function (ContainerConfigurator $containerConfigurator): void {
+    if ('dev' === $containerConfigurator->env()) {
+        $containerConfigurator->extension('zenstruck_foundry', [
+            'auto_refresh_proxies' => true,
+        ]);
+    }
+    if ('test' === $containerConfigurator->env()) {
+        $containerConfigurator->extension('zenstruck_foundry', [
+            'auto_refresh_proxies' => true,
+        ]);
+    }
+};
