96cf62df99901b0c9db3b14b0ecec7648ac9968a
Thomas Agé <thomas.age@protonmail.com>
2026-09-20 20:47:47 -0400

    chore(vendors): Upgrade
    - Upgrading doctrine/collections (2.6.0 => 3.1.0)
    - Upgrading doctrine/doctrine-bundle (3.3.1 => 3.3.2)
    - Upgrading doctrine/lexer (3.0.1 => 3.0.2)
    - Upgrading doctrine/orm (3.6.8 => 3.7.1)
    - Upgrading friendsofphp/php-cs-fixer (v3.95.18 => v3.95.26)
    - Upgrading monolog/monolog (3.10.0 => 3.12.0)
    - Upgrading nikic/php-parser (v5.8.0 => v5.9.0)
    - Upgrading pagerfanta/core (v4.8.0 => v4.9.0)
    - Upgrading pagerfanta/doctrine-orm-adapter (v4.8.0 => v4.9.0)
    - Upgrading pagerfanta/twig (v4.8.0 => v4.9.0)
    - Upgrading phpstan/phpstan (2.2.8 => 2.2.14)
    - Upgrading phpunit/php-file-iterator (6.0.1 => 6.0.2)
    - Upgrading phpunit/phpunit (12.5.33 => 12.5.35)
    - Upgrading rector/rector (2.6.2 => 2.6.7)
    - Upgrading sebastian/diff (7.0.0 => 7.0.1)
    - Upgrading symfony/browser-kit (v8.1.1 => v8.1.5)
    - Upgrading symfony/cache (v8.1.4 => v8.1.7)
    - Upgrading symfony/config (v8.1.4 => v8.1.5)
    - Upgrading symfony/console (v8.1.4 => v8.1.7)
    - Upgrading symfony/css-selector (v8.1.0 => v8.1.6)
    - Upgrading symfony/dependency-injection (v8.1.4 => v8.1.7)
    - Upgrading symfony/doctrine-bridge (v8.1.4 => v8.1.7)
    - Upgrading symfony/dom-crawler (v8.1.1 => v8.1.5)
    - Upgrading symfony/dotenv (v8.1.2 => v8.1.6)
    - Upgrading symfony/error-handler (v8.1.2 => v8.1.5)
    - Upgrading symfony/event-dispatcher (v8.1.2 => v8.1.5)
    - Upgrading symfony/filesystem (v8.1.2 => v8.1.6)
    - Upgrading symfony/finder (v8.1.1 => v8.1.7)
    - Upgrading symfony/form (v8.1.4 => v8.1.7)
    - Upgrading symfony/framework-bundle (v8.1.4 => v8.1.7)
    - Upgrading symfony/http-client (v8.1.4 => v8.1.7)
    - Upgrading symfony/http-client-contracts (v3.7.1 => v3.7.3)
    - Upgrading symfony/http-foundation (v8.1.4 => v8.1.7)
    - Upgrading symfony/http-kernel (v8.1.4 => v8.1.7)
    - Upgrading symfony/intl (v8.1.4 => v8.1.5)
    - Upgrading symfony/mailer (v8.1.2 => v8.1.7)
    - Upgrading symfony/maker-bundle (v1.67.0 => 1.68.0)
    - Upgrading symfony/messenger (v8.1.4 => v8.1.7)
    - Upgrading symfony/mime (v8.1.4 => v8.1.7)
    - Upgrading symfony/monolog-bridge (v8.1.2 => v8.1.6)
    - Upgrading symfony/monolog-bundle (v4.0.2 => v4.1.0)
    - Upgrading symfony/phpunit-bridge (v8.1.4 => v8.1.6)
    - Upgrading symfony/polyfill-deepclone (v1.40.0 => v1.42.0)
    - Upgrading symfony/polyfill-intl-idn (v1.38.1 => v1.42.0)
    - Upgrading symfony/polyfill-intl-normalizer (v1.38.0 => v1.42.0)
    - Locking symfony/polyfill-php86 (v1.41.0)
    - Upgrading symfony/process (v8.1.0 => v8.1.7)
    - Upgrading symfony/property-info (v8.1.4 => v8.1.7)
    - Upgrading symfony/rate-limiter (v8.1.4 => v8.1.6)
    - Upgrading symfony/redis-messenger (v8.1.4 => v8.1.7)
    - Upgrading symfony/routing (v8.1.2 => v8.1.6)
    - Upgrading symfony/security-bundle (v8.1.2 => v8.1.7)
    - Upgrading symfony/security-core (v8.1.2 => v8.1.6)
    - Upgrading symfony/security-http (v8.1.1 => v8.1.7)
    - Upgrading symfony/service-contracts (v3.7.1 => v3.7.3)
    - Upgrading symfony/string (v8.1.2 => v8.1.7)
    - Upgrading symfony/translation (v8.1.4 => v8.1.5)
    - Upgrading symfony/twig-bridge (v8.1.4 => v8.1.7)
    - Upgrading symfony/twig-bundle (v8.1.2 => v8.1.7)
    - Upgrading symfony/type-info (v8.1.0 => v8.1.5)
    - Upgrading symfony/uid (v8.1.4 => v8.1.5)
    - Upgrading symfony/validator (v8.1.4 => v8.1.7)
    - Upgrading symfony/var-dumper (v8.1.2 => v8.1.7)
    - Upgrading symfony/var-exporter (v8.1.4 => v8.1.6)
    - Upgrading symfony/web-profiler-bundle (v8.1.4 => v8.1.7)
    - Upgrading symfony/yaml (v8.1.2 => v8.1.6)
    - Upgrading twig/extra-bundle (v3.24.0 => v3.29.0)
    - Upgrading twig/intl-extra (v3.26.0 => v3.29.0)
    - Upgrading twig/twig (v3.28.0 => v3.29.0)
    - Upgrading zenstruck/browser (v1.10.1 => v1.12.0)
    - Upgrading zenstruck/foundry (v2.11.3 => v2.13.0)

--- public/index.php
+++ public/index.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 use App\Kernel;
 
-require_once dirname(__DIR__) . '/vendor/autoload_runtime.php';
+require_once __DIR__ . '/../vendor/autoload_runtime.php';
 
 return static function (array $context) {
     assert(is_string($context['APP_ENV'] ?? null));
